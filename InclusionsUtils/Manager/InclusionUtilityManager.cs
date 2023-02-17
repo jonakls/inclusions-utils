f3f0aef0889542c229ca76977f29b6d3d2dc8770
Jonathan Narvaez <89610430+jonakls@users.noreply.github.com>
2023-02-17 14:10:15 -0500

    refactor(defendants): rename variable of defendants collection

--- InclusionsUtils/Manager/InclusionUtilityManager.cs
+++ InclusionsUtils/Manager/InclusionUtilityManager.cs
@@ -8,9 +8,8 @@ namespace InclusionsUtils.Manager
         public Dictionary<string, string> ConvertParts(String parts)
         {
             Dictionary<string, string> cachedParts = new Dictionary<string, string>();
-            List<String> defandants = new List<String>();
+            List<String> defendants = new List<String>();
             List<String> complainants = new List<String>();
-
 
             String[] partsArray = parts.Replace("\r", "").Split('\n');
             foreach (String part in partsArray)
@@ -22,7 +21,7 @@ namespace InclusionsUtils.Manager
                 switch(realPart[0].ToLower())
                 {
                     case "demandado":
-                        defandants.Add(realPart[1]);
+                        defendants.Add(realPart[1]);
                         break;
                     case "demandante":
                         complainants.Add(realPart[1]);
@@ -30,7 +29,7 @@ namespace InclusionsUtils.Manager
                 }
             }
 
-            cachedParts["DEMANDADO"] = string.Join(" | ", defandants);
+            cachedParts["DEMANDADO"] = string.Join(" | ", defendants);
             cachedParts["DEMANDANTE"] = string.Join(" | ", complainants);
 
             return cachedParts;

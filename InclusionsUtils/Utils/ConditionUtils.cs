fbe7495fa1e267a9c9c8766f8556327fd8f2a6f0
jonakls <asmot54@hotmail.com>
2023-02-28 23:45:36 -0500

    feat(condition): add functionally for evaluate parts of demands

---
+++ InclusionsUtils/Utils/ConditionUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InclusionsUtils.Utils
+{
+    public static class ConditionUtils
+    {
+
+        public static bool IsDefendants(String section)
+        {
+            return section.ToLower().Contains("demandado")
+                   || section.ToLower().Contains("acusado")
+                   || section.ToLower().Contains("demandada");
+        }
+        
+        public static bool IsComplainants(String section)
+        {
+            return section.ToLower().Contains("demandante")
+                   || section.ToLower().Contains("accionante")
+                   || section.ToLower().Contains("actor")
+                   || section.ToLower().Contains("deudor");
+        }
+        
+    }
+}

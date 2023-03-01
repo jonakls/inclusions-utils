a0e445e65e466d73eb22a186841f3dbf47741ed9
jonakls <asmot54@hotmail.com>
2023-02-28 23:45:05 -0500

    feat(buttons): add button for about form

--- InclusionsUtils/Dashboard.Designer.cs
+++ InclusionsUtils/Dashboard.Designer.cs
@@ -34,13 +34,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.filings = new System.Windows.Forms.Button();
             this.fixParts = new System.Windows.Forms.Button();
+            this.resolveButton = new System.Windows.Forms.Button();
+            this.aboutMe = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 112);
+            this.label1.Location = new System.Drawing.Point(7, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(386, 33);
             this.label1.TabIndex = 0;
@@ -52,7 +54,7 @@
             this.partsConverter.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
             this.partsConverter.FlatAppearance.BorderSize = 2;
             this.partsConverter.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.partsConverter.Location = new System.Drawing.Point(102, 172);
+            this.partsConverter.Location = new System.Drawing.Point(101, 92);
             this.partsConverter.Name = "partsConverter";
             this.partsConverter.Size = new System.Drawing.Size(199, 32);
             this.partsConverter.TabIndex = 1;
@@ -64,7 +66,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(112, 363);
+            this.label2.Location = new System.Drawing.Point(111, 400);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(178, 13);
             this.label2.TabIndex = 4;
@@ -76,11 +78,11 @@
             this.filings.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
             this.filings.FlatAppearance.BorderSize = 2;
             this.filings.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.filings.Location = new System.Drawing.Point(102, 248);
+            this.filings.Location = new System.Drawing.Point(101, 168);
             this.filings.Name = "filings";
             this.filings.Size = new System.Drawing.Size(199, 32);
             this.filings.TabIndex = 5;
-            this.filings.Text = "RADICADOS";
+            this.filings.Text = "&RADICADOS";
             this.filings.UseVisualStyleBackColor = true;
             this.filings.Click += new System.EventHandler(this.button2_Click_1);
             // 
@@ -90,7 +92,7 @@
             this.fixParts.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
             this.fixParts.FlatAppearance.BorderSize = 2;
             this.fixParts.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fixParts.Location = new System.Drawing.Point(102, 210);
+            this.fixParts.Location = new System.Drawing.Point(101, 130);
             this.fixParts.Name = "fixParts";
             this.fixParts.Size = new System.Drawing.Size(199, 32);
             this.fixParts.TabIndex = 6;
@@ -98,11 +100,41 @@
             this.fixParts.UseVisualStyleBackColor = true;
             this.fixParts.Click += new System.EventHandler(this.fixParts_Click);
             // 
+            // resolveButton
+            // 
+            this.resolveButton.AutoSize = true;
+            this.resolveButton.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.resolveButton.FlatAppearance.BorderSize = 2;
+            this.resolveButton.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resolveButton.Location = new System.Drawing.Point(101, 206);
+            this.resolveButton.Name = "resolveButton";
+            this.resolveButton.Size = new System.Drawing.Size(199, 32);
+            this.resolveButton.TabIndex = 7;
+            this.resolveButton.Text = "R&ESUELVES";
+            this.resolveButton.UseVisualStyleBackColor = true;
+            this.resolveButton.Click += new System.EventHandler(this.resolveButton_Click);
+            // 
+            // aboutMe
+            // 
+            this.aboutMe.AutoSize = true;
+            this.aboutMe.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.aboutMe.FlatAppearance.BorderSize = 2;
+            this.aboutMe.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutMe.Location = new System.Drawing.Point(101, 348);
+            this.aboutMe.Name = "aboutMe";
+            this.aboutMe.Size = new System.Drawing.Size(199, 32);
+            this.aboutMe.TabIndex = 8;
+            this.aboutMe.Text = "ACERCA DE";
+            this.aboutMe.UseVisualStyleBackColor = true;
+            this.aboutMe.Click += new System.EventHandler(this.aboutMe_Click);
+            // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(411, 385);
+            this.ClientSize = new System.Drawing.Size(405, 422);
+            this.Controls.Add(this.aboutMe);
+            this.Controls.Add(this.resolveButton);
             this.Controls.Add(this.fixParts);
             this.Controls.Add(this.filings);
             this.Controls.Add(this.label2);
@@ -116,6 +148,10 @@
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button aboutMe;
+
+        private System.Windows.Forms.Button resolveButton;
 
         #endregion
 
--- InclusionsUtils/Dashboard.cs
+++ InclusionsUtils/Dashboard.cs
@@ -23,7 +23,7 @@ namespace InclusionsUtils
             filingsForm.Show();
             Hide();
         }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
             // Conversor de partes
@@ -38,12 +38,30 @@ namespace InclusionsUtils
             FixPartsForm fixPartsForm = new FixPartsForm(_manager);
             fixPartsForm.Show();
             Hide();
+            /*
+            * en InclusionsUtils.Form.FixPartsForm.InitializeComponent() en D:\Programación\RiderProjects\inclusions-utils\InclusionsUtils\Form\FixPartsForm.Designer.cs:línea 159
+            * en InclusionsUtils.Form.FixPartsForm..ctor(InclusionUtilityManager manager) en D:\Programación\RiderProjects\inclusions-utils\InclusionsUtils\Form\FixPartsForm.cs:línea 15
+            * en InclusionsUtils.Dashboard.fixParts_Click(Object sender, EventArgs e) en D:\Programación\RiderProjects\inclusions-utils\InclusionsUtils\Dashboard.cs:línea 38
+            */
         }
 
         private void Dashboard_FormClosing(object sender, FormClosingEventArgs e)
         {
             // Pre cerrado de este form
             Application.Exit();
+        }
+
+        private void resolveButton_Click(object sender, EventArgs e)
+        {
+            FixResolvesForm fixResolvesForm = new FixResolvesForm(_manager);
+            fixResolvesForm.Show();
+            Hide();
+        }
+
+        private void aboutMe_Click(object sender, EventArgs e)
+        {
+            new AboutForm(_manager).Show();
+            Hide();
         }
     }
 }

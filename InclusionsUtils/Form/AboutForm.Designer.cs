4dd82645aa35ed4d8285a338747aa400ca7dd4a5
jonakls <asmot54@hotmail.com>
2023-02-28 23:44:26 -0500

    feat(about): add about information for project code and author information

---
+++ InclusionsUtils/Form/AboutForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿using System.ComponentModel;
+
+namespace InclusionsUtils.Form
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AuthorLabel = new System.Windows.Forms.LinkLabel();
+            this.ProjectLabel = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SupportLabel = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LicenceLabel = new System.Windows.Forms.LinkLabel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "JONATHAN NARVAEZ | 2023 - 2024";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(386, 33);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "UTILIDADES INCLUSIONES";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 16);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "CREADO POR:";
+            // 
+            // AuthorLabel
+            // 
+            this.AuthorLabel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AuthorLabel.Location = new System.Drawing.Point(148, 126);
+            this.AuthorLabel.Name = "AuthorLabel";
+            this.AuthorLabel.Size = new System.Drawing.Size(154, 16);
+            this.AuthorLabel.TabIndex = 31;
+            this.AuthorLabel.TabStop = true;
+            this.AuthorLabel.Text = "Jonathan Narvaez";
+            this.AuthorLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.AuthorLabel_LinkClicked);
+            // 
+            // ProjectLabel
+            // 
+            this.ProjectLabel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProjectLabel.Location = new System.Drawing.Point(148, 152);
+            this.ProjectLabel.Name = "ProjectLabel";
+            this.ProjectLabel.Size = new System.Drawing.Size(154, 16);
+            this.ProjectLabel.TabIndex = 33;
+            this.ProjectLabel.TabStop = true;
+            this.ProjectLabel.Text = "Codigo Fuente";
+            this.ProjectLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.ProjectLabel_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 16);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "GITHUB:";
+            // 
+            // SupportLabel
+            // 
+            this.SupportLabel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SupportLabel.Location = new System.Drawing.Point(148, 177);
+            this.SupportLabel.Name = "SupportLabel";
+            this.SupportLabel.Size = new System.Drawing.Size(154, 16);
+            this.SupportLabel.TabIndex = 35;
+            this.SupportLabel.TabStop = true;
+            this.SupportLabel.Text = "Solicitud de soporte";
+            this.SupportLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.SupportLabel_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(44, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 16);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "SOPORTE:";
+            // 
+            // LicenceLabel
+            // 
+            this.LicenceLabel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LicenceLabel.Location = new System.Drawing.Point(148, 204);
+            this.LicenceLabel.Name = "LicenceLabel";
+            this.LicenceLabel.Size = new System.Drawing.Size(154, 16);
+            this.LicenceLabel.TabIndex = 37;
+            this.LicenceLabel.TabStop = true;
+            this.LicenceLabel.Text = "Acuerdo de licencia";
+            this.LicenceLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LicenceLabel_LinkClicked);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(44, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 16);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "LICENCIA:";
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 297);
+            this.Controls.Add(this.LicenceLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.SupportLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ProjectLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.AuthorLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AboutForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Acerca de";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AboutForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.LinkLabel LicenceLabel;
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.LinkLabel SupportLabel;
+
+        private System.Windows.Forms.LinkLabel ProjectLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel SupportLink;
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel AuthorLabel;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ InclusionsUtils/Form/AboutForm.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Windows.Forms;
+using InclusionsUtils.Manager;
+
+namespace InclusionsUtils.Form
+{
+    public partial class AboutForm : System.Windows.Forms.Form
+    {
+        private readonly InclusionUtilityManager _manager;
+        private readonly string _authorLink = "https://github.com/jonakls";
+        private readonly string _projectLink = "https://github.com/jonakls/inclusions-utils";
+        private readonly string _supportLink = "https://api.whatsapp.com/send?phone=3138494150&text=Hola%20jonathan,%20tengo%20un%20fallo%20con%20el%20programa%20de%20inclusiones";
+        private readonly string _licenceLink = "https://github.com/jonakls/inclusions-utils/blob/master/LICENSE";
+        
+        public AboutForm(InclusionUtilityManager manager)
+        {
+            InitializeComponent();
+            _manager = manager;
+        }
+
+        private void AboutForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            new Dashboard(_manager).Show();
+        }
+
+        private void AuthorLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(_authorLink);
+        }
+
+        private void ProjectLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(_projectLink);
+        }
+
+
+        private void SupportLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(_supportLink);
+        }
+
+        private void LicenceLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(_licenceLink);
+        }
+    }
+}
